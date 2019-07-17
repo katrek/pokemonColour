eb194bef6b8bd75699d8811fe32dd3c3252d78eb
Artem <akatrek@gmail.com>
2019-07-17 15:04:34 +0300

    added error handling but works like shit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, abort
+from flask import Flask, render_template, request, redirect
 import requests
 
 app = Flask(__name__)
@@ -10,19 +10,24 @@ def pokemon():
     if request.method == 'POST' and 'pokecolour' in request.form:
         colour = request.form.get('pokecolour')
         pokemon = get_poke_colour(colour)
-
-
     return render_template('index.html',
-                           pokemon=pokemon)
+                            pokemon = pokemon,)
+
+
+
 
 
 def get_poke_colour(colour):
-    r = requests.get('https://pokeapi.co/api/v2/pokemon-color/' + colour.lower())
-    pokedata = r.json()
-    pokemon = []
-    for i in pokedata['pokemon_species']:
-        pokemon.append(i['name'])
-    return pokemon
-
+    error = 'Not Found'
+    try:
+        r = requests.get('https://pokeapi.co/api/v2/pokemon-color/' + colour.lower())
+        pokedata = r.json()
+        pokemon = []
+        for i in pokedata['pokemon_species']:
+            pokemon.append(i['name'])
+        return pokemon
+    except Exception:
+        return error
 if __name__ == '__main__':
     app.run()
+app.secret_key = 'notsoeasy'
